4d98a43fbea3374da7235b9f37ce6ae7354338c5
Bryan Shears <brshears@gmail.com>
2022-08-03 16:19:34 -0600

    Adds views & update method to controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,8 @@
 class Admin::MerchantsController < ApplicationController
   def index
     @merchants = Merchant.all
+    @enabled_merchants = @merchants.where(status: "enabled")
+    @disabled_merchants = @merchants.where(status: "disabled")
   end
 
   def show
@@ -13,7 +15,15 @@ class Admin::MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    flash.notice = "Merchant information has been updated"
-    redirect_to "/admin/merchants/#{merchant.id}"
+
+    if params[:status]
+      merchant.update(status: params[:status])
+      redirect_to "/admin/merchants"
+    else
+      merchant.update(name: params[:name])
+      flash.notice = "Merchant information has been updated"
+      redirect_to "/admin/merchants/#{merchant.id}"
+    end
+    
   end
 end
